2a226a70e21665551fadb86e213e20aef1a0a59b
Desertsnowman <Desertsnowman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-25 19:45:32 +0000

    primary release
    git-svn-id: https://plugins.svn.wordpress.org/db-toolkit/trunk@280740 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- data_form/fieldtypes/onoff/javascript.php
+++
@@ -1,9 +0,0 @@
-<?php 
-// add any additional javascript required for the field type.
-?>//<script>
-
-function onoff_toggleInline(eid, field, id){
-	ajaxCall('onoff_setValue', jQuery('#onOff_'+eid+'_'+id).attr('checked'), id, field, eid, function(o){
-		dr_goToPage(eid, false);
-	})
-}
--- data_form/fieldtypes/vodashop/output.php
+++
@@ -1,3 +0,0 @@
-<?php
-	$Out .= '<div class="'.$Row.'"><strong>'.$FieldTitle.'</strong><br />'.nl2br($Data[$Field]).'</div>';
-?>
