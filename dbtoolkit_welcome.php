20b24f5886cc34cf072bb3a0062747aa9c9b061e
Desertsnowman <Desertsnowman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-22 20:18:17 +0000

    git-svn-id: https://plugins.svn.wordpress.org/db-toolkit/trunk@349396 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ dbtoolkit_welcome.php
@@ -0,0 +1,85 @@
+<div class="wrap dbtoolkit">
+<div id="icon-themes" class="icon32"></div><h2>DB-Toolkit
+<a class="button add-new-h2" href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=PA8U68HRBXTEU&lc=ZA&item_name=dbtoolkit%20development&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donate_LG%2egif%3aNonHosted" target="_blank">Donate</a>
+</h2>
+
+<div class="postbox-container" style="width:32.6%">
+    <div class="metabox-holder">
+        <div id="dashboard_right_now" class="postbox">
+            <h3><span>From the DB-Toolkit Blog</span></h3>
+            <div id="dbtoolkitBlogFeed" style="margin: 10px;">
+            Loading....
+            </div>
+        </div>
+    </div>
+    <div class="metabox-holder">
+        <div id="dashboard_right_now" class="postbox">
+            <h3><span>From the DB-Toolkit Support Forum</span></h3>
+            <div id="dbtoolkitSupportFeed" style="margin: 10px;">
+            Loading....
+            </div>
+        </div>
+    </div>
+</div>
+<div class="postbox-container" style="width:32.6%">
+    <div class="metabox-holder">
+        <div id="dashboard_right_now" class="postbox">
+            <h3><span>Help DB-Toolkit Grow!</span></h3>
+            <div style="margin: 10px;">
+                <p>DB-Toolkit takes a lot of time to build and being only developed by a single person, this time is precious.</p>
+                <p>If you like DB-Toolkit and feel its worth something and should be developed further, show your support with a little donation. Any amounts are appreciated.</p>
+                <p>
+                    <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+                    <input type="hidden" name="cmd" value="_s-xclick">
+                    <input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHPwYJKoZIhvcNAQcEoIIHMDCCBywCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYBjp858jIJde2Lkkc25rvHcfRbTTlv8NXVY1I7Mw+HRiYcQ1tGfvLgXtIO6Z4AbHlBYGTAcVDaIX1ubCYxSqduymmFiWOxjV8+qqbcx20G7LKAH/qwp+zcHFcqn1SV2hM6JW3dCHnws2l74qEXCk3MT6R/ASVpmZYe3/JotAomXkTELMAkGBSsOAwIaBQAwgbwGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQIBu0ullawlkqAgZjY1mZidWQvUvxMgy4QYk62/BbEs5SnSyofmdOF/Y5S6zquucDxsJOWcOyEdCEnYP9MXYd9o9ZowDG73b59FnMYfrewuO9uCstSAfLc04sQNzeq5UWiJt911iwJ3OQB8lOkwK0s10qyuoxB/ldV3RJP/A/FqRQexSyxVozx/MVdn/VuWFN/cujJZy+ZPDzvAOuFPU7yYvlkXaCCA4cwggODMIIC7KADAgECAgEAMA0GCSqGSIb3DQEBBQUAMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTAeFw0wNDAyMTMxMDEzMTVaFw0zNTAyMTMxMDEzMTVaMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEAwUdO3fxEzEtcnI7ZKZL412XvZPugoni7i7D7prCe0AtaHTc97CYgm7NsAtJyxNLixmhLV8pyIEaiHXWAh8fPKW+R017+EmXrr9EaquPmsVvTywAAE1PMNOKqo2kl4Gxiz9zZqIajOm1fZGWcGS0f5JQ2kBqNbvbg2/Za+GJ/qwUCAwEAAaOB7jCB6zAdBgNVHQ4EFgQUlp98u8ZvF71ZP1LXChvsENZklGswgbsGA1UdIwSBszCBsIAUlp98u8ZvF71ZP1LXChvsENZklGuhgZSkgZEwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tggEAMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEFBQADgYEAgV86VpqAWuXvX6Oro4qJ1tYVIT5DgWpE692Ag422H7yRIr/9j/iKG4Thia/Oflx4TdL+IFJBAyPK9v6zZNZtBgPBynXb048hsP16l2vi0k5Q2JKiPDsEfBhGI+HnxLXEaUWAcVfCsQFvd2A1sxRr67ip5y2wwBelUecP3AjJ+YcxggGaMIIBlgIBATCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwCQYFKw4DAhoFAKBdMBgGCSqGSIb3DQEJAzELBgkqhkiG9w0BBwEwHAYJKoZIhvcNAQkFMQ8XDTExMDIxNzExNTc1NVowIwYJKoZIhvcNAQkEMRYEFEU7jZEgUSsiffKms1YmbtF5fcGuMA0GCSqGSIb3DQEBAQUABIGAnNY6v8L3V2MXBArGoUBA2H5SSuodp+UQHTEH0lxmMdIbUhkfJpEdKkbAe0jkFURmAYUzkcJfny1iRQxLBlt55vccXIIYu/FVVboeCkjIOGQ35C+QhDh1YidGDXy1hzmz05fgMgvMeVKziN8uE1Yld0hxjV09qM/TE4jqN810Aas=-----END PKCS7-----
+                    ">
+                    <input type="image" src="https://www.paypal.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+                    <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+                    </form>
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="metabox-holder">
+        <div id="dashboard_right_now" class="postbox">
+            <h3><span>New Features in This Version (0.2.2.4)</span></h3>
+            <div style="margin: 10px;">
+                
+                <p><strong>Insert Interface Editor Button</strong>: You can now insert an interface Shortcode using the Insert Interface button on the text editor toolbar.</p>
+                <p><strong>PDF Exporting Update</strong>: Added PDF Templates. Currently not customisable but will be in next release</p>
+                <p><strong>Charting</strong>: Fixed a bug that prevented Charting from being enabled. in the Charting Tab, you can enable charting..</p>
+                <p><strong>Import Button</strong>: when creating an interface, in the settings tab, check the "<strong>Show Import</strong>" Button to enable the import feature.</p>
+                <p><strong>Dashboard</strong>: This page right here. in the next release i will show stats on the interfaces and application in db-toolkit.</p>
+                <p><strong>Access Control</strong>: Preparation have been made for access control mechanism that will allow you to make pages and interfaces be shown only to users with qualifying access rights.</p>
+                <p><strong>Marketplace</strong>: Preparation have been made for interface publishing to the db-toolkit marketplace. Here you'll be able to share and download ready made interfaces and applications.</p>
+                <p><strong>BugFixes</strong>: The all important bug fixes, mostly minor.</p>
+                
+            </div>
+        </div>
+    </div>
+</div>
+
+</div>
+
+<script>
+
+jQuery(document).ready(function($) {
+
+    ajaxCall('core_loadSupportFeed','http://dbtoolkit.digilab.co.za/forum/rss/', function(o){
+       jQuery('#dbtoolkitSupportFeed').html(o);
+    });
+
+    ajaxCall('core_loadSupportFeed','http://dbtoolkit.digilab.co.za/feed/', function(o){
+       jQuery('#dbtoolkitBlogFeed').html(o);
+    });
+
+});
+
+</script>
+
+
+
+
+
+
+
