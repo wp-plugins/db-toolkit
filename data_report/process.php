0ad3465c216a9591d8d4bf48b1fd28ecc9a42e20
Desertsnowman <Desertsnowman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-22 18:32:54 +0000

    git-svn-id: https://plugins.svn.wordpress.org/db-toolkit/trunk@349327 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ data_report/process.php
@@ -0,0 +1,48 @@
+<div id="tabs-formprocess">
+
+    <div class="stuffbox">
+        <h3>Form Processors</h3>
+        <div class="admin_config_toolbar">
+            <ul class="tools_widgets">
+                <li class="root_item"><a class="parent"><strong>Processors</strong></a>
+                    <ul id="" style="visibility: hidden; display: block;">
+
+                        <?php
+                            echo df_listProcessors();
+                        ?>
+
+                    </ul>
+                </li>
+            </ul>
+            <div style="clear:both;"></div>
+        </div>
+        <div class="inside">
+            <p>Once a form is submitted, Form processors will filter through the data submitted and manipulate it in various ways.</p>
+            <p>Processes run in order they are presented below, from top to bottom and are stacked. therefore every process will include the changes the previous process has applied.</p>
+            <div id="formProcessList" class="columnSorter ui-sortable">
+                <?php
+                echo df_buildSetProcessors($Element['Content'])
+                ?>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+$_SESSION['dataform']['OutScripts'] .= "
+
+    // activate menus
+    jQuery('.tools_widgets ul').css({
+        display: \"none\"
+    });
+    jQuery('.tools_widgets li').hover(function(){
+        jQuery(this).find('ul:first').css({
+            visibility: \"visible\",
+            display: \"none\"
+        }).fadeIn(250);
+    },function(){
+        jQuery(this).find('ul:first').css({
+            visibility: \"hidden\"
+        });
+    });
+";
+?>
