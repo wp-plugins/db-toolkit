2b33c6e45507f769cda569bbfefc9ea0bc4a7323
Desertsnowman <Desertsnowman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-23 20:37:47 +0000

    3.1 compatibility
    git-svn-id: https://plugins.svn.wordpress.org/db-toolkit/trunk@349988 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- data_form/fieldtypes/text/footer.php
+++ data_form/fieldtypes/text/footer.php
@@ -1,1 +1,3 @@
-<script type="text/javascript" src="system/dais/plugins/data_form/fieldtypes/text/jwysiwyg/jquery.wysiwyg.js"></script>
+<?php 
+//<script type="text/javascript" src="system/dais/plugins/data_form/fieldtypes/text/jwysiwyg/jquery.wysiwyg.js"></script>
+?>
