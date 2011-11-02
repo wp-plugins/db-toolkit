6399f50a45f7ff2ba5d5ed07fbe1197e8b4178c6
Desertsnowman <Desertsnowman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-02 12:44:54 +0000

    git-svn-id: https://plugins.svn.wordpress.org/db-toolkit/trunk@458630 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- data_report/processors/googlemap/conf.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* 
- * emailer form processor
- * sends out the form data as an email
- */
-    $ViewTitle = 'Google Map';
-    $ViewDesc = 'Plots Data to a Google Map.';
-
-?>
--- data_report/processors/googlemap/styles.php
+++
@@ -1,7 +0,0 @@
-<?php
-// included in the styles location
-// use wp_enqueue_style in here
-
-wp_register_script($handle, $src);
-
-?>
