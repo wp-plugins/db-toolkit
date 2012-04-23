31ab2b4bebcc3521b6d047aac387b1ec9514fc6e
Desertsnowman <Desertsnowman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-23 14:46:42 +0000

    git-svn-id: https://plugins.svn.wordpress.org/db-toolkit/tags/0.3.2.021@535093 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- plugincore.php
+++ plugincore.php
@@ -4,7 +4,7 @@ Plugin Name: DB-Toolkit
 Plugin URI: http://dbtoolkit.co.za
 Description: Plugin for building databases, managers and viewers for custom Content Management.
 Author: David Cramer
-Version: 0.3.2.020
+Version: 0.3.2.021
 Author URI: http://dbtoolkit.co.za
 */
 
